6a7c9b0e3385d46713488f6a1706f19868eea9c3
Toyin <oluwatoyinogundare@gmail.com>
2020-05-12 02:58:43 -0600

    Polymorphism completed

--- encapsulation.cs
+++ encapsulation.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 
 namespace encapsulation
 {
@@ -69,3 +69,4 @@ namespace encapsulation
         }
     }
     }
+*/
---
+++ polymorphism.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace polymorphism
+{
+    class Program
+    {
+        class Painting
+        {
+            public virtual void paintingDetails()
+            {
+                Console.WriteLine("[This are all the details of our painting]");
+            }
+        }
+    class PaintingName: Painting
+    {
+        public override void paintingDetails()
+        {
+            Console.WriteLine("Name is atmosphere");
+        }
+    }
+
+    class PaintingArtist: Painting
+    {
+        public override void paintingDetails()
+        {
+            Console.WriteLine("Artist is Taiwo-Toyin");
+        }
+    }
+
+    class PaintingMedium: Painting
+    {
+        public override void paintingDetails()
+        {
+            Console.WriteLine("Medium is pencil sketch");
+        }
+    }
+
+    class PaintingYear: Painting
+    {
+        public override void paintingDetails()
+        {
+            Console.WriteLine("Year is 1766");
+        }
+    }
+
+    static void Main(string[] args)
+        {
+            Painting myPainting =  new Painting();
+            Painting myPaintingName = new PaintingName();
+            Painting myPaintingArtist = new PaintingArtist();
+            Painting myPaintingMedium = new PaintingMedium();
+            Painting myPaintingYear = new PaintingYear();
+
+            myPainting.paintingDetails();
+            myPaintingName.paintingDetails();
+            myPaintingArtist.paintingDetails();
+            myPaintingMedium.paintingDetails();
+            myPaintingYear.paintingDetails();
+        }
+    }
+
+    
+}
